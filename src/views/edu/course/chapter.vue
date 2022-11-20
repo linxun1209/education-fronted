539e0c2f55eaaa9cf50bf7ca1fd77cc44b9d2b67
linxun1209 <3056726345@qq.com>
2022-11-20 11:14:28 +0800

    章节的添加

--- src/views/edu/course/chapter.vue
+++ src/views/edu/course/chapter.vue
@@ -1,54 +1,69 @@
 <template>
+
   <div class="app-container">
+
     <h2 style="text-align: center;">发布新课程</h2>
 
     <el-steps :active="2" process-status="wait" align-center style="margin-bottom: 40px;">
-      <el-step title="填写课程基本信息" />
-      <el-step title="创建课程大纲" />
-      <el-step title="最终发布" />
+      <el-step title="填写课程基本信息"/>
+      <el-step title="创建课程大纲"/>
+      <el-step title="最终发布"/>
     </el-steps>
+
     <el-button type="text" @click="openChapterDialog()">添加章节</el-button>
+
     <!-- 章节 -->
     <ul class="chanpterList">
       <li
         v-for="chapter in chapterVideoList"
-        :key="chapter.id"
-      >
+        :key="chapter.id">
         <p>
           {{ chapter.title }}
 
           <span class="acts">
-            <el-button style="" type="text" @click="openVideo(chapter.id)">添加小节</el-button>
-            <el-button style="" type="text" @click="openEditChatper(chapter.id)">编辑</el-button>
-            <el-button type="text" @click="removeChapter(chapter.id)">删除</el-button>
-          </span>
+                    <el-button style="" type="text" @click="openVideo(chapter.id)">添加小节</el-button>
+                    <el-button style="" type="text" @click="openEditChatper(chapter.id)">编辑</el-button>
+                    <el-button type="text" @click="removeChapter(chapter.id)">删除</el-button>
+                </span>
         </p>
 
         <!-- 视频 -->
         <ul class="chanpterList videoList">
           <li
             v-for="video in chapter.children"
-            :key="video.id"
-          >
+            :key="video.id">
             <p>{{ video.title }}
 
               <span class="acts">
 
-                <el-button style="" type="text">编辑</el-button>
-                <el-button type="text" @click="removeVideo(video.id)">删除</el-button>
-              </span>
+                    <el-button style="" type="text">编辑</el-button>
+                    <el-button type="text" @click="removeVideo(video.id)">删除</el-button>
+                </span>
             </p>
           </li>
         </ul>
       </li>
     </ul>
-    <el-form label-width="120px">
+    <div>
+      <el-button @click="previous">上一步</el-button>
+      <el-button :disabled="saveBtnDisabled" type="primary" @click="next">下一步</el-button>
+    </div>
 
-      <el-form-item>
-        <el-button @click="previous">保存并下一步</el-button>
-        <el-button :disabled="saveBtnDisabled" type="primary" @click="next">保存并下一步</el-button>
-      </el-form-item>
-    </el-form>
+    <!-- 添加和修改章节表单 -->
+    <el-dialog :visible.sync="dialogChapterFormVisible" title="添加章节">
+      <el-form :model="chapter" label-width="120px">
+        <el-form-item label="章节标题">
+          <el-input v-model="chapter.title"/>
+        </el-form-item>
+        <el-form-item label="章节排序">
+          <el-input-number v-model="chapter.sort" :min="0" controls-position="right"/>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogChapterFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="saveOrUpdate">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
--- src/views/edu/course/info.vue
+++ src/views/edu/course/info.vue
@@ -118,11 +118,45 @@ export default {
     }
   },
   created() {
-    // 初始化所有讲师
-    this.getListTeacher()
-    // 初始化一级分类
-    this.getOneSubject()
-  }, methods: {
+    // 获取路由id值
+    if (this.$route.params && this.$route.params.id) {
+      this.courseId = this.$route.params.id
+      // 调用根据id查询课程的方法
+      this.getInfo()
+    } else {
+      // 初始化所有讲师
+      this.getListTeacher()
+      // 初始化一级分类
+      this.getOneSubject()
+    }
+  },
+  methods: {
+    // 根据课程id查询
+    getInfo() {
+      course.getCourseInfoId(this.courseId)
+        .then(response => {
+          // 在courseInfo课程基本信息，包含 一级分类id 和 二级分类id
+          this.courseInfo = response.data.courseInfoVo
+          // 1 查询所有的分类，包含一级和二级
+          subject.getSubjectList()
+            .then(response => {
+              // 2 获取所有一级分类
+              this.subjectOneList = response.data.list
+              // 3 把所有的一级分类数组进行遍历，
+              for (var i = 0; i < this.subjectOneList.length; i++) {
+                // 获取每个一级分类
+                var oneSubject = this.subjectOneList[i]
+                // 比较当前courseInfo里面一级分类id和所有的一级分类id
+                if (this.courseInfo.subjectParentId === oneSubject.id) {
+                  // 获取一级分类所有的二级分类
+                  this.subjectTwoList = oneSubject.children
+                }
+              }
+            })
+            // 初始化所有讲师
+          this.getListTeacher()
+        })
+    },
     // 上传封面成功调用的方法
     handleAvatarSuccess(res, file) {
       this.courseInfo.cover = res.data.url
@@ -170,7 +204,8 @@ export default {
           this.teacherList = response.data.items
         })
     },
-    saveOrUpdate() {
+    // 添加课程
+    addCourse() {
       course.addCourseInfo(this.courseInfo)
         .then(response => {
           // 提示
@@ -181,6 +216,28 @@ export default {
           // 跳转到第二步
           this.$router.push({ path: '/course/chapter/' + response.data.courseId })
         })
+    },
+    // 修改课程
+    updateCourse() {
+      course.updateCourseInfo(this.courseInfo)
+        .then(response => {
+          // 提示
+          this.$message({
+            type: 'success',
+            message: '修改课程信息成功!'
+          })
+          // 跳转到第二步
+          this.$router.push({ path: '/course/chapter/' + this.courseId })
+        })
+    },
+    saveOrUpdate() {
+      // 判断添加还是修改
+      if (!this.courseInfo.id) {
+        // 添加
+        this.addCourse()
+      } else {
+        this.updateCourse()
+      }
     }
   }
 }
