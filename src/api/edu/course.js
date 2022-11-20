97b2b814ff7ce3683ef3ed89ab2e896d3fc397a8
linxun1209 <3056726345@qq.com>
2022-11-20 16:30:16 +0800

    视频上传功能

--- src/api/edu/course.js
+++ src/api/edu/course.js
@@ -8,10 +8,10 @@ export default {
       data: courseInfo
     })
   },
-  // 2 查询所有讲师
+  // 查询所有讲师
   getListTeacher() {
     return request({
-      url: '/eduservice/teacher/findAll',
+      url: `/eduservice/teacher/findAll`,
       method: 'get'
     })
   },
@@ -29,5 +29,51 @@ export default {
       method: 'post',
       data: courseInfo
     })
+  },
+  // 根据课程id查询
+  updateCourseInfo(courseInfo) {
+    return request({
+      url: '/eduservice/course/getPublishCourseInfo' + id,
+      method: 'get'
+    })
+  },
+  // 课程确认信息显示
+  getPublishCourseInfo(id) {
+    return request({
+      url: '/eduservice/course/getPublishCourseInfo/' + id,
+      method: 'get'
+    })
+  },
+  // 课程最终发布
+  publihCourse(id) {
+    return request({
+      url: '/eduservice/course/publishCourse/' + id,
+      method: 'post'
+    })
+  },
+  // TODO 课程列表
+  // 课程最终发布
+  getListCourse() {
+    return request({
+      url: '/eduservice/course',
+      method: 'get'
+    })
+  },
+  removeCourse(id) {
+    return request({
+      url: `/eduservice/course/${id}`,
+      method: 'delete'
+    })
+  },
+  getCourseListPage(current, limit, courseQuery) {
+    return request({
+      url: `/eduservice/course/pageCourseCondition/${current}/${limit}`,
+      method: 'post',
+      // courseQuery条件对象，后端用requestbody获取数据
+      // data表示把对象转换成json数据传递到接口里面
+
+      data: courseQuery
+    })
   }
+
 }
